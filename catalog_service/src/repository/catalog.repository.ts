ccfc6c6fc652bc573c1ec70f57ce2fa7da6c4936
TitanSarim <muhammadsarim203@gmail.com>
2025-01-18 05:17:58 -0800

    boiler plate nodejs ts

---
+++ catalog_service/src/repository/catalog.repository.ts
@@ -0,0 +1,20 @@
+import { ICatalogRepository } from "../interface/catalogRepository";
+import { Product } from "../models/product.model";
+
+export class CatalogRepository implements ICatalogRepository {
+  create(data: any): Promise<Product> {
+    throw new Error("Method not implemented.");
+  }
+  update(data: any): Promise<Product> {
+    throw new Error("Method not implemented.");
+  }
+  delete(id: any): Promise<boolean> {
+    throw new Error("Method not implemented.");
+  }
+  find(): Promise<Product[]> {
+    throw new Error("Method not implemented.");
+  }
+  findOne(id: number): Promise<Product> {
+    throw new Error("Method not implemented.");
+  }
+}
