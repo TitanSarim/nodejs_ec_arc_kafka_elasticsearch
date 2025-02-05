d3b759ac3d22b80373dab3145dfe269555c6d328
TitanSarim <muhammadsarim203@gmail.com>
2025-02-05 05:14:59 -0800

    docker and crud operation completed

--- catalog_service/src/repository/catalog.repository.ts
+++ catalog_service/src/repository/catalog.repository.ts
@@ -1,26 +1,36 @@
+import { PrismaClient } from "@prisma/client";
 import { ICatalogRepository } from "../interface/catalogRepository";
 import { Product } from "../models/product.model";
-import { ProductFactory } from "../utils/fixtures";
 
 export class CatalogRepository implements ICatalogRepository {
+  _primsa: PrismaClient;
+
+  constructor() {
+    this._primsa = new PrismaClient();
+  }
+
   async create(data: any): Promise<Product> {
-    const product = ProductFactory.build();
-    return Promise.resolve(product);
+    return this._primsa.product.create({ data });
   }
   async update(data: any): Promise<Product> {
-    const product = ProductFactory.build();
-    return Promise.resolve(product);
+    return this._primsa.product.update({ where: { id: data.id }, data });
   }
   async delete(id: any): Promise<number> {
-    const product = ProductFactory.build();
-    return Promise.resolve(id);
+    const data = await this._primsa.product.delete({ where: { id: id } });
+    return Promise.resolve(data.id);
   }
   async find(limit: number, offset: number): Promise<Product[]> {
-    const products = ProductFactory.buildList(limit);
-    return Promise.resolve(products);
+    return this._primsa.product.findMany({
+      take: limit,
+      skip: offset,
+      orderBy: { id: "asc" },
+    });
   }
-  findOne(id: number): Promise<Product> {
-    const product = ProductFactory.build();
-    return Promise.resolve(product);
+  async findOne(id: number): Promise<Product> {
+    const data = await this._primsa.product.findFirst({ where: { id: id } });
+    if (!data) {
+      throw new Error(`Product not found`);
+    }
+    return Promise.resolve(data);
   }
 }
